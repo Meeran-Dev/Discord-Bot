c33c6a46e59b6e88fb893bf805b658e873533701
Mohd Meeran <mdmeeran.86684@gmail.com>
2025-09-11 14:06:15 +0530

    Added bot.py for the bot script

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
