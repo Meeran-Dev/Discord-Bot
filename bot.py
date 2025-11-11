d1d8e0b4463b749e7ad7d5b84eef2da033358ed4
Mohd Meeran <mdmeeran.86684@gmail.com>
2025-11-11 17:16:05 +0530

    Re-did the code too use discord.ext commands

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import random
 import os
 from dotenv import load_dotenv
@@ -14,37 +15,46 @@ slap_gifs = ["https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExczN4OW84cTZsc2R4O
              "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExczN4OW84cTZsc2R4OXFudWZnMTZyZ3F4bWxiY3dtb29ob3NuamJuOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Zau0yrl17uzdK/giphy.gif",
              "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExczN4OW84cTZsc2R4OXFudWZnMTZyZ3F4bWxiY3dtb29ob3NuamJuOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/xUNd9HZq1itMkiK652/giphy.gif"]
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-
-        if message.author == self.user:
-            return
-
-        if message.content.startswith('>bot punch'):
-            if message.mentions:
-                other_user = message.mentions[0]
-                gif = random.choice(punch_gifs)
-                caption = f'Ouch! {message.author.display_name} punched {other_user.display_name}. \n{gif}'
-
-                embed = discord.Embed(
-                    title=caption,
-                    color=discord.Color.brand_red()
-                )
-
-                embed.set_image(url = gif)
-                await message.channel.send(embed=embed)
-
-            else:
-                await message.channel.send(f'Mention the user you want to punch!')
-
-
-
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='>bot ', intents=intents)
 
-client = MyClient(intents=intents)
-client.run(token)
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
+
+@bot.command()
+async def punch(ctx, member: discord.Member = None):
+    if member is None:
+        await ctx.send(f'Mention the user you want to punch!')
+        return
+
+    gif = random.choice(punch_gifs)
+    caption = f'Ouch! {ctx.author.display_name} punched {member.display_name}.'
+
+    embed = discord.Embed(
+        title=caption,
+        color=discord.Color.brand_red()
+    )
+
+    embed.set_image(url=gif)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def slap(ctx, member: discord.Member = None):
+    if member is None:
+        await ctx.send(f'Mention the user you want to slap!')
+        return
+
+    gif = random.choice(slap_gifs)
+    caption = f'BAM! {ctx.author.display_name} slapped {member.display_name}.'
+
+    embed = discord.Embed(
+        title=caption,
+        color=discord.Color.blue()
+    )
+
+    embed.set_image(url=gif)
+    await ctx.send(embed=embed)
+
+bot.run(token)
